950f71cfe0a76b0e03c541321879e3b0a80c42ea
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-02-28 22:39:55 +0530

    feat: add another post and fix some things

--- src/config.ts
+++ src/config.ts
@@ -7,20 +7,19 @@ export const SITE = {
   ogImage: "devosfera-og.webp", // located in the public folder
   lightAndDarkMode: true,
   postPerIndex: 6,
-  postPerPage: 8,
+  postPerPage: 12,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showGalleries: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit this post",
-    url: "https://github.com/0xdres/astro-devosfera/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "America/Guatemala", // Default global timezone (IANA format)
+  timezone: "Asia/Kolkata", // Default global timezone (IANA format)
   introAudio: {
     enabled: false, // show/hide the player in the hero section
     src: "/audio/intro-web.mp3", // path to the file (relative to /public)
